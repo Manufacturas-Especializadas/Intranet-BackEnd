1a4be7b9a0dbc7a9ba6bb384ceb577da06167d27
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-10 14:25:51 -0600

    Modelo y Dto de los colaboradores destacados

---
+++ Intranet/Dtos/FeaturedCollaboratorsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Intranet.Dtos
+{
+    public class FeaturedCollaboratorsDto
+    {
+        public string Name { get; set; }
+
+        public string Testimonial { get; set; }
+
+        public IFormFile Photo { get; set; }
+    }
+}
