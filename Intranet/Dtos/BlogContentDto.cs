9cc5dd0cfe71e9b2b1b694df6e5fed234baf5e36
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-13 15:47:05 -0600

    Modelo y Dto del blog

---
+++ Intranet/Dtos/BlogContentDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Intranet.Dtos
+{
+    public class BlogContentDto
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public string Template { get; set; }
+
+        public IFormFile Img { get; set; }
+
+    }
+}
