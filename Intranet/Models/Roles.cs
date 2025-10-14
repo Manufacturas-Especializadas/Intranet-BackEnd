6862cc7070547279e5332828b396763bb51a7a64
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-14 08:38:07 -0600

    Acepta nulos

--- Intranet/Models/Roles.cs
+++ Intranet/Models/Roles.cs
@@ -7,7 +7,7 @@ public partial class Roles
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     public virtual ICollection<Users> Users { get; set; } = new List<Users>();
 }
