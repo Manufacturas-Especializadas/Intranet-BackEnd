0d09fd97622e2aa6c84cecfdd23cf12f04c22306
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-10-17 14:57:08 -0600

    Acepta nulo

--- Intranet/Dtos/BlogContentDto.cs
+++ Intranet/Dtos/BlogContentDto.cs
@@ -2,13 +2,13 @@
 {
     public class BlogContentDto
     {
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
-        public string Content { get; set; }
+        public string? Content { get; set; }
 
-        public string SubTitle { get; set; }
+        public string? SubTitle { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public string Template { get; set; }
 
