52e10c6c97841af8e0d716e45dbd6cb8eccd970a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-07 14:00:57 -0600

    Nuevos modelos

---
+++ Intranet/Models/BlogMedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intranet.Models;
+
+public partial class BlogMedia
+{
+    public int Id { get; set; }
+
+    public string Url { get; set; }
+
+    public string MediaType { get; set; }
+
+    public int BlogContentId { get; set; }
+
+    public virtual BlogContent BlogContent { get; set; }
+}
