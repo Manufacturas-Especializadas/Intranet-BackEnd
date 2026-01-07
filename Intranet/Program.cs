1ae6533ef8689bd16489546743cec6373439311f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-07 14:01:55 -0600

    Configuracion de Desiarilizar json

--- Intranet/Program.cs
+++ Intranet/Program.cs
@@ -3,11 +3,14 @@ using Intranet.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                    .AddJsonOptions(x =>
+                        x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
